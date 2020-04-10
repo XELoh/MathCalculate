4f006f64f194c53073c23b99f46b84fbf1936d28
Ethan <ethanloh@trinax.sg>
2020-04-10 17:38:11 +0800

    hello

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -27,7 +27,7 @@ public class MathCalculator extends CordovaPlugin {
                 int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
                 int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
 
-                callback.success("" + (p1 + p2));
+                callback.success("" + (p1 + p1));
             } catch (Exception ex) {
                 callback.error("Something went wrong " + ex);
             }
